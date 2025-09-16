7e5f77f6be0b3f08e735d8267a01501fc293aa89
sunilcodingpro <sunilcodingpro@gmail.com>
2025-09-15 23:31:52 -0500

    Add setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="ExplainableAIforClinicalDecisionSupportSystems",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    description="Explainable AI for Clinical Decision Support Systems",
+    author="sunilcodingpro",
+)
